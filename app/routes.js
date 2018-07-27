a9952f293d0d01afcc50e82dd2e3432dae3df4f6
Ben Wong <ben.wong@unboxedconsulting.com>
2018-07-27 19:05:49 +0100

    search by string

--- app/routes.js
+++ app/routes.js
@@ -12,7 +12,23 @@ router.get('/', function (req, res) {
 // Add your routes here - above the module.exports line
 
 router.get('/search', function (req, res) {
-  res.render('search-v1/index', { policies: search.policies })
+  let policies = search.policies.sort(
+                                (p1, p2) => {
+                                   let n1 = p1.Policy_full_name.toLowerCase()
+                                   let n2 = p2.Policy_full_name.toLowerCase()
+                                   return (n1 === n2 ? 0 : (n1 < n2 ? -1 : 1))
+                                }
+                              )
+  if (req.query.q) {
+    let term = req.query.q.toLowerCase()
+    policies = policies.filter((policy) => {
+      return policy.Policy_full_name.toLowerCase().indexOf(term) >= 0
+    })
+  }
+
+  res.render('search-v1/index', {
+    policies: policies
+  })
 })
 
 module.exports = router
