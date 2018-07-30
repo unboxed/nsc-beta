7e217a6d327e673f416bc10b9804ad36bf224191
Ben Wong <ben.wong@unboxedconsulting.com>
2018-07-30 14:51:39 +0100

    search and filtering forms

--- app/routes.js
+++ app/routes.js
@@ -14,20 +14,35 @@ router.get('/', function (req, res) {
 router.get('/search', function (req, res) {
   let policies = search.policies.sort(
                                 (p1, p2) => {
-                                   let n1 = p1.Policy_full_name.toLowerCase()
-                                   let n2 = p2.Policy_full_name.toLowerCase()
+                                   let n1 = p1.Condition.toLowerCase()
+                                   let n2 = p2.Condition.toLowerCase()
                                    return (n1 === n2 ? 0 : (n1 < n2 ? -1 : 1))
                                 }
                               )
-  if (req.query.q) {
-    let term = req.query.q.toLowerCase()
+  if (req.query.term && req.query.term.trim() !== "" ) {
+    let term = req.query.term.toLowerCase()
     policies = policies.filter((policy) => {
-      return policy.Policy_full_name.toLowerCase().indexOf(term) >= 0
+      return policy.Condition.toLowerCase().indexOf(term) >= 0
+    })
+  }
+
+  if (req.query.who_filter && req.query.who_filter !== "None") {
+    policies = policies.filter((policy) => {
+      return req.query.who_filter.split(",").includes(policy.Type)
+    })
+  }
+
+  if (req.query.status_filter&& req.query.status_filter !== "None") {
+    policies = policies.filter((policy) => {
+      return req.query.status_filter.split(",").includes(policy.Recommendation_filter)
     })
   }
 
   res.render('search-v1/index', {
-    policies: policies
+    policies: policies,
+    who_filter: req.query.who_filter || 'None',
+    status_filter: req.query.status_filter || 'None',
+    term: req.query.term
   })
 })
 
