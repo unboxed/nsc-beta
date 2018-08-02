61483fc0264ad8aeb08e2228d8a7564fb236979e
Ben Wong <ben.wong@unboxedconsulting.com>
2018-08-02 11:38:03 +0100

    Have 2 versions of search one without filters
    Also add placeholder text

--- app/routes.js
+++ app/routes.js
@@ -11,34 +11,21 @@ router.get('/', function (req, res) {
 
 // Add your routes here - above the module.exports line
 
-router.get('/search', function (req, res) {
-  let policies = search.policies.sort(
-                                (p1, p2) => {
-                                   let n1 = p1.Condition.toLowerCase()
-                                   let n2 = p2.Condition.toLowerCase()
-                                   return (n1 === n2 ? 0 : (n1 < n2 ? -1 : 1))
-                                }
-                              )
-  if (req.query.term && req.query.term.trim() !== "" ) {
-    let term = req.query.term.toLowerCase()
-    policies = policies.filter((policy) => {
-      return policy.Condition.toLowerCase().indexOf(term) >= 0
-    })
-  }
+router.get('/search_a', function (req, res) {
+  let policies = search.filter(req.query)
+  res.render('search-v1/index_a', {
+    service_name_override: ' ',
+    policies: policies,
+    who_filter: req.query.who_filter || 'None',
+    status_filter: req.query.status_filter || 'None',
+    term: req.query.term
+  })
+})
 
-  if (req.query.who_filter && req.query.who_filter !== "None") {
-    policies = policies.filter((policy) => {
-      return req.query.who_filter.split(",").includes(policy.Type)
-    })
-  }
-
-  if (req.query.status_filter&& req.query.status_filter !== "None") {
-    policies = policies.filter((policy) => {
-      return req.query.status_filter.split(",").includes(policy.Recommendation_filter)
-    })
-  }
-
-  res.render('search-v1/index', {
+router.get('/search_b', function (req, res) {
+  let policies = search.filter(req.query)
+  res.render('search-v1/index_b', {
+    service_name_override: ' ',
     policies: policies,
     who_filter: req.query.who_filter || 'None',
     status_filter: req.query.status_filter || 'None',
--- app/search.js
+++ app/search.js
@@ -1,4 +1,4 @@
-exports.policies = [
+const data = [
   {
     "URL": "aaa",
     "Condition": "Abdominal Aortic Aneurysm ",
@@ -1078,3 +1078,33 @@ exports.policies = [
     "": null
   }
 ]
+
+exports.filter = function (query) {
+  let policies = data.sort(
+    (p1, p2) => {
+      let n1 = p1.Condition.toLowerCase()
+      let n2 = p2.Condition.toLowerCase()
+      return (n1 === n2 ? 0 : (n1 < n2 ? -1 : 1))
+    }
+  )
+  if (query.term && query.term.trim() !== '') {
+    let term = query.term.toLowerCase()
+    policies = policies.filter((policy) => {
+      return policy.Condition.toLowerCase().indexOf(term) >= 0
+    })
+  }
+
+  if (query.who_filter && query.who_filter !== 'None') {
+    policies = policies.filter((policy) => {
+      return query.who_filter.split(',').includes(policy.Type)
+    })
+  }
+
+  if (query.status_filter && query.status_filter !== 'None') {
+    policies = policies.filter((policy) => {
+      return query.status_filter.split(',').includes(policy.Recommendation_filter)
+    })
+  }
+
+  return policies
+}
