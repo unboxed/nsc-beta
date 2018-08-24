21a7b7013e1e70f834d8c1e8e8ae17c82a546390
Celia Collins <celiadcollins@gmail.com>
2018-08-24 16:20:24 +0100

    Altered SMA page and added link to homepage

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'NSC Admin Dashboard',
+  serviceName: ' ',
 
   // Default port that prototype runs on
   port: '3000',
